9ca9602622c3fa1044dff6020caaf9ae02569e78
Giuseppe Caponetto <giuseppe.caponetto@foodiestrip.com>
2017-09-10 22:24:05 +0200

    updated production port nr

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.use(express.static('build'));
-
-app.listen(5000, () => {
-  console.log('r3pi test app listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`r3pi test app listening on port ${port}!`);
 });
